1547e0304e8a286287bda49d34e27bba780d06bb
Aliph0th <theblacklord05@gmail.com>
2024-08-03 20:41:10 +0300

    add: regex parsing

---
+++ src/regex/index.ts
@@ -0,0 +1,30 @@
+import { ParsingToken } from '../types';
+import * as regex from './expressions';
+
+export function checkExports(code: string): ParsingToken[] {
+   const checks = regex.checkExpressions.map(expression => checkRegex(code, expression));
+
+   return checks.reduce<ParsingToken[]>((prev, curr) => {
+      if (curr) {
+         prev.push(...curr);
+      }
+      return prev;
+   }, []);
+}
+
+function checkRegex(code: string, expression: RegExp) {
+   const matches = code.matchAll(expression);
+   const result: ParsingToken[] = [];
+   for (const match of matches) {
+      if (match.groups) {
+         const token: ParsingToken = {
+            names: match.groups.names.trim().replace(regex.alias, '').split(regex.splitter)
+         };
+         match.groups.path && (token.exportPath = match.groups.path);
+         result.push(token);
+      }
+   }
+   return result.length ? result : null;
+}
+
+export { regex };
